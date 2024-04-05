ce4ff4460541fb0f5851478012af3996d09d3e88
Hamza Husic <hamzahusic.dev@gmail.com>
2024-04-05 08:40:15 +0200

    Added car route in app.js

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require('cors');
 const userRoutes = require('./routes/userRoutes')
+const carRoutes = require('./routes/carRoutes')
 
 app.use(express.json());
 app.use(cors());
@@ -9,6 +10,7 @@ app.use(cors());
 const PORT = 8000;
 
 app.use("/user",userRoutes);
+app.use("/car",carRoutes);
 
 app.listen(PORT, () => {
     console.log("Server is listening to port " + PORT)
--- server/routes/carRoutes.js
+++ server/routes/carRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const mysqlPool = require('../db/connection');
+const mysqlPool = require('../connection');
 const multer = require("multer");
 const router = express.Router();
 const fs = require("fs");
